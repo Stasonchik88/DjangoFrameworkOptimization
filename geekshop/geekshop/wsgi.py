722f76f87a27840eab6e1cd731b1c254af485087
Stasonchik <poselenov_stas@mail.ru>
2022-03-22 23:10:31 +0300

    Update wsgi.py

--- geekshop/geekshop/wsgi.py
+++ geekshop/geekshop/wsgi.py
@@ -7,10 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
-
+import os, sys
+sys.path.insert(0, '/home/s/stasonchik/stasonchik.beget.tech/DjangoFrameworkOptimization/geekshop')
+sys.path.insert(1, '/home/s/stasonchik/stasonchik.beget.tech/.beget/tmp/Python-3.9.5/.venv_gb_store/lib/python3.9/site-packages')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'geekshop.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geekshop.settings')
-
 application = get_wsgi_application()
